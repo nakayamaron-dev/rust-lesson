993fddd17e624c51e96000f032d9707f9bc192cb
Ryo Nakayama <nakayamadevelop@gmail.com>
2021-09-18 19:22:26 +0900

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-mod vars;
+// mod stack_heap;
+// mod vars;
+mod ownership;
 
 fn main() {
-    println!("Hello, world!");
-    vars::run();
-    vars::sub_a::func_a();
-    vars::sub_b::func_b();
+    // vars::run();
+    // stack_heap::run();
+    // vars::sub_a::func_a();
+    // vars::sub_b::func_b();
+    ownership::run();
 }
